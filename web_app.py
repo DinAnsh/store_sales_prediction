a36842d4e4dabef22bee047aef0c4e5e3644ba27
DinAnsh <54740603+DinAnsh@users.noreply.github.com>
2022-08-02 20:16:57 +0530

    version 2.0

--- web_app.py
+++ web_app.py
@@ -1,7 +1,7 @@
 # doing necessary imports
 import numpy as np
 from flask import Flask, request, render_template
-import pickle   
+import pickle
 import log
 import Encoding
 import store_predictions
@@ -13,43 +13,45 @@ app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home_page():
     log.put_log(2, "Open Home page")
-    return render_template('index.html')
+    return render_template('home.html')
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
     try:
         log.put_log(2, "Predict func called")
         user_values = list(request.form.values())
         log.put_log(2, "Got values from user")
         features = Encoding.get_features(user_values)
+
         log.put_log(2, "Values encoded")
         try:
             final_features = [np.array(features)]
             prediction = model.predict(final_features)
-        except:
+        except Exception as e:
             log.put_log(3, " There is some error!!")
-            return f"There is an ERROR - {features}"
+            return f"There is an ERROR - {e}"
         log.put_log(2, "Values predicted")
 
-        output = round(prediction[0], 4)
+        output = prediction[0]
         store_predictions.write_in_file(user_values, output)
 
         log.put_log(2, f"Got output {output}")
-        return render_template('index.html', prediction_text='Item Outlet Sales Should be $ {}'.format(output**4))
+        prediction = round(output ** 4, 4)
+        log.put_log(2, "Prediction Successfull!")
+        return render_template('result.html', prediction=prediction)
 
     except Exception as e:
-        log.put_log(3, " There is some error!!")
+        log.put_log(3, "There is some error!!")
         return f"There is an ERROR in prediction - {e} !!"
-
-    finally:
-        log.put_log(2, "Prediction Successfull!")
 
 
 if __name__ == "__main__":
